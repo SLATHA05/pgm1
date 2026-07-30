121a6538a858ba891b62edfd679021105e91a9bd
S.LATHA <lathas@rvsgroup.com>
2026-07-30 15:30:40 +0530

    Create main.py

---
+++ main.py
@@ -0,0 +1,25 @@
+# ==========================================
+# Assignment: Travel Company Profit Calculator
+# Name:
+# Register Number:
+# ==========================================
+
+# Problem:
+# A travel company wants to fly a plane to Coimbatore.
+# Flying the plane costs 5000 dollars.
+# 29 people have signed up for the trip.
+# Each ticket costs 200 dollars.
+#
+# Create variables for:
+# 1. Flight cost
+# 2. Number of passengers
+# 3. Ticket price
+#
+# Calculate:
+# - Total Revenue
+# - Profit
+#
+# Display the profit.
+
+# Write your code below
+
